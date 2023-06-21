4451d73393930622dda6acad9844a86d07408ba1
“skaat-mf” <nigthaviator@gmail.com>
2023-06-21 23:23:25 +0300

    Creating and initializing the database

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+docker run \
+   --name sde-pg \
+   -e POSTGRES_PASSWORD="@sde_password012" \
+   -e POSTGRES_USER="test_sde" \
+   -e POSTGRES_DB="demo" \
+   -p 5432:5432 \
+   -v $(pwd)/sql/init_db:/docker-entrypoint-initdb.d \
+   -v $(pwd)/sql:/$HOME/sql \
+   -d postgres:15.3-alpine
